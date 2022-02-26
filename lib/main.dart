ddd6a84745fe7c4dd772bc57c77dd3fbceca7a68
CerionSel <daniil.selyutin@gmail.com>
2022-02-26 15:34:13 +0300

    sizer, firebase (before flutter upgrade)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,42 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sizer/sizer.dart';
 
+import 'firebase_options.dart';
 
-void main() {}
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+
+  runApp(Sizer(
+    builder: (context, orientation, deviceType) {
+      return const MaterialApp(
+        home: MyApp(),
+      );
+    }
+  ));
+}
+
+class MyApp extends StatefulWidget{
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp>{
+  @override
+  Widget build(BuildContext context) {
+    FirebaseCrashlytics.instance.crash();
+    return Container();
+  }
+}
