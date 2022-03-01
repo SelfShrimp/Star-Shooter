ded0a576b9cf6f663632b8773ea354eb4a1722da
CerionSel <daniil.selyutin@gmail.com>
2022-03-01 19:58:41 +0300

    окно поражения и перезапуск игры

--- lib/game_core/game.dart
+++ lib/game_core/game.dart
@@ -1,16 +1,19 @@
 import 'dart:isolate';
 
 import 'package:flutter/material.dart';
+import 'package:starshooter/scene/defeat_scene.dart';
 import 'package:starshooter/scene/play_scene.dart';
 
 import 'main_loop.dart';
 
 class Game extends StatefulWidget {
+  const Game({Key? key}) : super(key: key);
+
   @override
   _GameState createState() => _GameState();
 }
 
-class _GameState extends State<Game>{
+class _GameState extends State<Game> {
   final ReceivePort _receivePort = ReceivePort(); //порт связи изолятов
   PlayScene _playScene = PlayScene();
 
@@ -28,6 +31,8 @@ class _GameState extends State<Game>{
           isolate.kill(priority: Isolate.immediate);
           _receivePort.close();
           _playScene = PlayScene(); //очищаем поле
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (context) => const Defeat()));
         }
       });
     }();
---
+++ lib/scene/defeat_scene.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:starshooter/game_core/main_loop.dart';
+import 'package:starshooter/main.dart';
+
+class Defeat extends StatelessWidget{
+  const Defeat({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/background.jpg"), fit: BoxFit.none)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Game Over'),
+          Container(
+            height: 10,
+          ),
+          ElevatedButton(
+              onPressed: (){
+                running = true;
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) => const MyApp()));
+              },
+              child: const Text('Retry?'))
+        ],
+      ),
+    );
+  }
+}
